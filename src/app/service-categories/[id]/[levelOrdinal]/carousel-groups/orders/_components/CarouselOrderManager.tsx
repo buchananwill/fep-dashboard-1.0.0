f1070a66929e2c163cffae04d2714d00f59a0a3f
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-05-28 09:51:59 +0100

    Written the diffing code for orderItem changes. Next: the code to add/remove assignees.

--- src/app/service-categories/[id]/[levelOrdinal]/carousel-groups/orders/_components/CarouselOrderManager.tsx
+++ src/app/service-categories/[id]/[levelOrdinal]/carousel-groups/orders/_components/CarouselOrderManager.tsx
@@ -1,6 +1,12 @@
 import { BaseDtoUiProps, NamespacedHooks } from 'dto-stores';
 import { CarouselOrderDto } from '@/api/dtos/CarouselOrderDtoSchema';
-import { useEffect, useRef } from 'react';
+import {
+  Dispatch,
+  MutableRefObject,
+  SetStateAction,
+  useEffect,
+  useRef
+} from 'react';
 import { EntityClassMap } from '@/api/entity-class-map';
 import { KEY_TYPES } from 'dto-stores/dist/literals';
 import { EmptyArray } from '@/api/main';
@@ -12,6 +18,55 @@ import { CarouselOptionDto } from '@/api/dtos/CarouselOptionDtoSchema';
 import { CarouselOptionState } from '@/app/service-categories/[id]/[levelOrdinal]/carousel-groups/orders/_components/CarouselOption';
 import { produce } from 'immer';
 import { isEqual } from 'lodash';
+import { UpdateAction } from 'selective-context/dist/types';
+import { CarouselOrderItemDto } from '@/api/dtos/CarouselOrderItemDtoSchema';
+
+interface WriteAny<T> {
+  (contextKey: string, proposedUpdate: SetStateAction<T>): void;
+}
+
+function handleAddAssignee(
+  dispatchWriteAny: WriteAny<CarouselOptionState>,
+  carouselOptionId: number
+) {}
+
+function handleRemoveAssignee(
+  dispatchWriteAny: WriteAny<CarouselOptionState>,
+  carouselOptionId: number
+) {}
+
+function performDiffOnItem(
+  orderItems: MutableRefObject<Record<string, CarouselOrderItemDto>>,
+  item: CarouselOrderItemDto,
+  dispatchWriteAny: WriteAny<CarouselOptionState>
+) {
+  const prevItem = orderItems.current[item.workProjectSeriesSchemaId];
+  const activeMatch = prevItem?.active === item.active;
+  const assignmentMatch = prevItem?.carouselOptionId === item.carouselOptionId;
+  // match: true true
+  if (activeMatch && assignmentMatch) return; // no need to update the ref
+  // match: true false
+  if (assignmentMatch) {
+    if (item.active) handleAddAssignee(dispatchWriteAny, item.carouselOptionId);
+    else handleRemoveAssignee(dispatchWriteAny, item.carouselOptionId);
+  }
+  // match: false true
+  else if (activeMatch) {
+    if (item.active) {
+      handleRemoveAssignee(dispatchWriteAny, prevItem?.carouselOptionId);
+      handleAddAssignee(dispatchWriteAny, item.carouselOptionId);
+    }
+    // (no action needed if the item WAS and IS inactive, apart from update ref)
+  }
+  // match: false false
+  else if (item.active) {
+    handleAddAssignee(dispatchWriteAny, item.carouselOptionId);
+  } else {
+    handleRemoveAssignee(dispatchWriteAny, prevItem?.carouselOptionId);
+  }
+  // finally: update the ref
+  orderItems.current[item.workProjectSeriesSchemaId] = item;
+}
 
 export default function CarouselOrderManager({
   entity
@@ -33,18 +88,17 @@ export default function CarouselOrderManager({
   const { dispatchWriteAny } = useGlobalWriteAny<CarouselOptionState>();
 
   useEffect(() => {
-    Object.values(entity.carouselOrderItems).forEach((item) => {
-      const prevItem = orderItems.current[item.workProjectSeriesSchemaId];
-      prevItem.active = item.active;
-      if (item.active) {
-        dispatchWriteAny(
-          `CarouselOrderState:${item.carouselOptionId}`,
-          (state) =>
-            produce(state, (draft) => {
-              draft.assignees.includes();
-            })
-        );
-      }
-    });
-  }, []);
+    Object.values(entity.carouselOrderItems).forEach((item) =>
+      performDiffOnItem(orderItems, item, dispatchWriteAny)
+    );
+  }, [dispatchWriteAny, entity.carouselOrderItems]);
 }
+
+/**
+ * 1. Add assignee
+ *    i. item is active
+ *    ii. list does not contain item
+ * 2. Remove assignee
+ *    i. item is inactive
+ *    ii. item is assigned elsewhere
+ * */
