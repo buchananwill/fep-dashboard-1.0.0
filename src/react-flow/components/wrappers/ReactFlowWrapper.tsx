7e79c9df49f75ea02d26fccffe7b38d5c822b274
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-07-22 00:00:14 +0100

    Found the missing styles import.

--- src/react-flow/components/wrappers/ReactFlowWrapper.tsx
+++ src/react-flow/components/wrappers/ReactFlowWrapper.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { PropsWithChildren } from 'react';
 
-import 'reactflow/dist/style.css';
+import '@xyflow/react/dist/style.css';
 
 export function ReactFlowWrapper({ children }: PropsWithChildren) {
   return <div style={{ width: '100vw', height: '100vh' }}>{children}</div>;
