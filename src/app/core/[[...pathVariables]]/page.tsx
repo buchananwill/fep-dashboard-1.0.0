77466e4c600a43261b6c248f113bcc9707240d2c
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-08-07 12:09:43 +0100

    Removing unused code.

--- src/app/core/[[...pathVariables]]/page.tsx
+++ src/app/core/[[...pathVariables]]/page.tsx
@@ -1,16 +1,11 @@
 import { navTreeData } from '@/app/core/navigation/navTreeData';
 import { ResolveNavTree } from '@/app/core/navigation/ResolveNavTree';
-import { auth } from '@/auth';
-import { LinkButton } from '@/app/service-categories/LinkButton';
 
 export default async function page({
   params: { pathVariables }
 }: {
   params: { pathVariables: string[] };
 }) {
-  // const session = await auth();
-  // if (!session)
-  //   return <LinkButton href={'/login'}>Go To Login Page</LinkButton>;
   return (
     <ResolveNavTree
       navTree={navTreeData}
