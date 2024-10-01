d7685f62552eac4c5a33bd10246fea763d81467a
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-10-01 18:06:30 +0100

    Chunked the work schema node list into columns and placed on a rootcard.

--- src/components/work-schema-nodes/WorkSchemaNodesHome.tsx
+++ src/components/work-schema-nodes/WorkSchemaNodesHome.tsx
@@ -3,20 +3,46 @@ import { LeafComponentProps } from '@/app/core/navigation/types';
 import WorkSchemaNodeRootGraph from '@/components/work-schema-nodes/WorkSchemaNodeRootGraph';
 import PathVariableSplit from '@/components/generic/PathVariableSplit';
 import { LinkButton } from '@/components/navigation/LinkButton';
+import { chunk, sortBy } from 'lodash';
+import RootCard from '@/app/core/navigation/RootCard';
+import { getRootCardLayoutId } from '@/components/work-task-types/getRootCardLayoutId';
+
+const DESIRED_COLUMN_NUMBER = 3;
 
 async function Home({ pathVariables }: LeafComponentProps) {
   const rootNodeList = await Api.WorkSchemaNode.getRootNodeList();
+  const sortedByNameThenId = sortBy(rootNodeList, [
+    (node) => node.name,
+    (node) => node.id
+  ]);
+  const chunkSize = Math.round(
+    sortedByNameThenId.length / DESIRED_COLUMN_NUMBER
+  );
+  const arrays = chunk(sortedByNameThenId, chunkSize);
 
   return (
-    <div className={'flex flex-col gap-2'}>
-      {rootNodeList.map((node) => (
-        <LinkButton
-          key={node.id}
-          href={`/core/${pathVariables.join('/')}/${node.id}`}
-        >
-          {node.name ?? `Work Schema Node: ${node.id}`}
-        </LinkButton>
-      ))}
+    <div className={'p-4'}>
+      <RootCard layoutId={getRootCardLayoutId(pathVariables)}>
+        <div className={'grid grid-cols-3 gap-4'}>
+          {arrays.map((list, index) => (
+            <div
+              key={index}
+              className={'flex flex-col gap-2  rounded-large shadow-small'}
+            >
+              {list.map((node) => {
+                return (
+                  <LinkButton
+                    href={`/core/${pathVariables.join('/')}/${node.id}`}
+                    key={node.id}
+                  >
+                    {node.name ?? `Work Schema Node: ${node.id}`}
+                  </LinkButton>
+                );
+              })}
+            </div>
+          ))}
+        </div>
+      </RootCard>
     </div>
   );
 }
