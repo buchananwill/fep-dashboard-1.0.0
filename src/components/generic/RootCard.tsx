1ae0e9bd1924aa3ff3f976808ed90681cf2ba704
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-10-19 00:03:07 +0100

    Re-writing the RootCard with MUI.

--- src/components/generic/RootCard.tsx
+++ src/components/generic/RootCard.tsx
@@ -1,16 +1,14 @@
 'use client';
-import { Card, CardBody, CardHeader } from '@nextui-org/card';
 import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/popover';
-import { Button } from '@nextui-org/button';
 import { NavLinkDescriptionsDefault } from '@/components/navigation/navLinkDescriptions';
 import { motion } from 'framer-motion';
 import {
   NavigationType,
   navLinkIcons
 } from '@/components/navigation/navLinkIcons';
-import { PropsWithChildren, ReactNode } from 'react';
-import { usePathname } from 'next/navigation';
+import { PropsWithChildren, ReactNode, useState } from 'react';
 import clsx from 'clsx';
+import { Card, CardHeader, CardContent, Button } from '@mui/material';
 
 export default function RootCard({
   layoutId,
@@ -26,6 +24,8 @@ export default function RootCard({
   const navLinkDescription = navigationType
     ? NavLinkDescriptionsDefault[navigationType]
     : null;
+
+  const [popoverOpen, setPopoverOpen] = useState(false);
 
   return (
     <motion.div
@@ -43,27 +43,33 @@ export default function RootCard({
         animate={{ scale: 1 }}
         transition={{ duration: 0.8 }}
       >
-        <Card className={'h-full'}>
+        <Card className={'h-full rounded-2xl'}>
           {displayHeader &&
             (navLinkDescription ? (
-              <Popover triggerScaleOnOpen={false}>
-                <PopoverTrigger>
-                  <Button
-                    variant={'light'}
-                    className={'rounded-none italic text-default-500'}
-                  >
-                    <CardHeader
-                      className={'flex justify-between  border-default-200 '}
+              <div
+                className={'flex w-full  justify-between border-default-200'}
+              >
+                <Popover
+                  triggerScaleOnOpen={false}
+                  onOpenChange={setPopoverOpen}
+                  isOpen={popoverOpen}
+                >
+                  <PopoverTrigger>
+                    <Button
+                      className={
+                        'w-full justify-between rounded-none italic text-default-500'
+                      }
+                      onClick={() => setPopoverOpen(true)}
                     >
                       {Icon && <Icon className={'h-8 w-8'} />}
                       {displayHeader}
-                    </CardHeader>
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className={'max-w-lg'}>
-                  {navigationType && navLinkDescription}
-                </PopoverContent>
-              </Popover>
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className={'max-w-lg'}>
+                    {navigationType && navLinkDescription}
+                  </PopoverContent>
+                </Popover>
+              </div>
             ) : (
               <CardHeader
                 className={'flex justify-between  border-default-200 '}
@@ -72,9 +78,9 @@ export default function RootCard({
                 {displayHeader}
               </CardHeader>
             ))}
-          <CardBody className={clsx(displayHeader && 'pt-0')}>
+          <CardContent className={clsx(displayHeader && 'pt-0')}>
             {children}
-          </CardBody>
+          </CardContent>
         </Card>
       </motion.div>
     </motion.div>
