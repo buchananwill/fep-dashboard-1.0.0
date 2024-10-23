f75c2acf91b46af5fc16b0cdc2fa0e6ff6ed70d7
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-10-23 16:12:26 +0100

    ProviderRoleSelectorTable.tsx refactored.

--- src/components/tables/selectorTables/ProviderRoleSelectorTable.tsx
+++ src/components/tables/selectorTables/ProviderRoleSelectorTable.tsx
@@ -1,15 +1,15 @@
 'use client';
 import React from 'react';
-import { Chip } from '@nextui-org/react';
-import FilterSelectEntityTable from '@/components/tables/FilterSelectEntityTable';
 import { ProviderRoleDto } from '@/api/generated-types/generated-types';
 
 import { Column } from '@/types';
 import { EntityClassMap } from '@/api/entity-class-map';
-import { getCellRenderFunction } from '@/components/tables/GetCellRenderFunction';
-import { SimpleValueToString } from '@/components/tables/SimpleValueToString';
 import { getDomainAlias } from '@/api/getDomainAlias';
-import { StringValueChip } from '@/components/tables/StringValueChip';
+import EntityTable from '@/components/tables/edit-tables/EntityTable';
+import { getCellRenderFunction } from '@/components/tables/cells-v2/GetCellRenderFunction';
+import { CellComponentRecord } from '@/components/tables/core-table-types';
+import { AnyValueToString } from '@/components/tables/cells-v2/AnyValueToString';
+import { Sorts } from '@/components/tables/cells-v2/DefaultSortStates';
 
 export default function ProviderRoleSelectorTable({
   entities
@@ -18,13 +18,12 @@ export default function ProviderRoleSelectorTable({
 }) {
   return (
     <>
-      <FilterSelectEntityTable
-        entities={entities}
-        initialColumns={ProviderColumnsInitial}
-        filterProperty={'partyName'}
-        renderCell={providerRoleCell}
+      <EntityTable
+        cellModel={providerRoleCell}
         columns={providerColumns}
         entityClass={EntityClassMap.providerRole}
+        defaultSort={Sorts.name}
+        withSelection
       />
     </>
   );
@@ -48,8 +47,13 @@ export const providerColumns: Column<ProviderRoleDto>[] = [
   }
 ];
 
-const providerRoleCell = getCellRenderFunction('providerRole', {
-  partyName: SimpleValueToString,
-  knowledgeDomainName: StringValueChip,
-  'type.name': SimpleValueToString
-});
+const cellComponentRecord: CellComponentRecord<ProviderRoleDto> = {
+  partyName: { type: 'IdInnerCell', component: AnyValueToString },
+  knowledgeDomainName: { type: 'IdInnerCell', component: AnyValueToString },
+  'type.name': { type: 'IdInnerCell', component: AnyValueToString }
+} as const;
+
+const providerRoleCell = getCellRenderFunction(
+  'providerRole',
+  cellComponentRecord
+);
