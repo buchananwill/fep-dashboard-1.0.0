181698f18da0c52ae7b78710bee5e2f4e5d483b7
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-12-20 11:42:00 +0000

    Changes modal confirmation footer to use success/danger semantic colors.

--- src/components/tables/cells-v2/specific/ModalConfirmationFooter.tsx
+++ src/components/tables/cells-v2/specific/ModalConfirmationFooter.tsx
@@ -13,10 +13,10 @@ export function ModalConfirmationFooter({
 }) {
   return (
     <div className={'center-all-margin flex w-fit gap-4 pt-4'}>
-      <Button color={'red'} variant={'subtle'} onClick={onCancel}>
+      <Button color={'danger'} variant={'subtle'} onClick={onCancel}>
         {cancelLabel ?? 'Cancel'}
       </Button>
-      <Button color={'primary'} onClick={onConfirm}>
+      <Button color={'success'} onClick={onConfirm}>
         {confirmLabel ?? 'Confirm'}
       </Button>
     </div>
